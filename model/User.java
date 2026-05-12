9c4d0dd2a03b5dc19fa9d5b7828ae01647765f38
DEEPIKA-web20 <deepikasakthivel20@gmail.com>
2026-05-12 21:08:42 +0530

    Create User.java

---
+++ model/User.java
@@ -0,0 +1,12 @@
+public class User {
+
+    String name;
+    String skill;
+    String contact;
+
+    public User(String name, String skill, String contact) {
+        this.name = name;
+        this.skill = skill;
+        this.contact = contact;
+    }
+}
