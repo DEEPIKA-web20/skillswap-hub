faf4d2e7b1a189fac55d53a54dc3d2528420f264
DEEPIKA-web20 <deepikasakthivel20@gmail.com>
2026-05-12 20:53:35 +0530

    Create Skill.java

---
+++ src/Skill.java
@@ -0,0 +1,12 @@
+public class Skill {
+
+    String name;
+    String skill;
+    String contact;
+
+    public Skill(String name, String skill, String contact) {
+        this.name = name;
+        this.skill = skill;
+        this.contact = contact;
+    }
+}
